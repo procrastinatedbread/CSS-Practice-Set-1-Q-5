6256f044b4f5bb56ceb67ef2941aff158b648917
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-23 00:29:06 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class="card">
+<img src="https://via.placeholder.com/350x150" alt="tasveer"/>
+<h2>Card title</h2>
+<p>This is an example description for the card</p>
+</div>
+`;
